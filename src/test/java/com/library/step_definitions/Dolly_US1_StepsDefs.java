044c42ccb6ec4e6a9fa9eca3018759a57abe32b5
DollyHaddad <haddad.dolly@yahoo.com>
2022-12-06 23:18:20 -0800

    US1 some other change

--- src/test/java/com/library/step_definitions/Dolly_US1_StepsDefs.java
+++ src/test/java/com/library/step_definitions/Dolly_US1_StepsDefs.java
@@ -20,26 +20,16 @@ public class Dolly_US1_StepsDefs {
     @When("Execute query to get all IDs from users")
     public void execute_query_to_get_all_i_ds_from_users() {
         String query1="select count(id) from users";
-
         DB_Util.runQuery(query1);
-
         actualResult = DB_Util.getFirstRowFirstColumn();
-
         System.out.println("actualResult = " + actualResult);
-
     }
     @Then("verify all users has unique ID")
     public void verify_all_users_has_unique_id() {
         String query2="select count(distinct id) from users";
-
-
         DB_Util.runQuery(query2);
-
         String expectedResult = DB_Util.getCellValue(1, 1);
-
         System.out.println("expectedResult = " + expectedResult);
-
-
         Assert.assertEquals(expectedResult,actualResult);
 
     }
--- src/test/resources/features/US1_dolly.feature
+++ src/test/resources/features/US1_dolly.feature
@@ -1,4 +1,4 @@
-@us01
+@usDolly
 Feature: As a data consumer, I want the user information are stored in mySql DB correctly in users table.
   Background:
     Given Establish the database connection
